7040d323221ce5f6a1331e3a7e39ccd86dee967b
deep1net <its.ahmed81@gmail.com>
2023-07-25 01:21:36 +0530

    question2.py
    Rotating matrix without using any library

---
+++ question2.py
@@ -0,0 +1,40 @@
+def rotate(matrix, n):
+    
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    # Reverse the rows
+    for i in range(n):
+        matrix[i].reverse()
+
+
+def matrix_input():
+    rows = int(input("Enter number of rows: "))
+    columns = int(input("Enter number of columns: "))
+
+    if rows != columns:
+        return ValueError("No. of rows is not equal to no. of columns")
+
+    matrix = []
+    for i in range(rows):
+        row_input = input(f"Enter elements of row {i+1}: ")
+        row_elements = list(map(int, row_input.split()))
+        matrix.append(row_elements)
+
+    return matrix, rows
+
+
+if __name__ == '__main__':
+          
+    # Matrix input
+    matrix, size = matrix_input()
+    print("Matrix before rotation:")
+    for row in matrix:
+        print(row)
+
+    rotate(matrix, size)
+
+    print("Matrix after rotation:")
+    for row in matrix:
+        print(row)
